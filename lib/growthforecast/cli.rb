13fd3ad88fedc1cb57041d232708916537e461aa
Naotoshi Seo <sonots@gmail.com>
2013-09-19 17:05:31 +0900

    add color command

--- lib/growthforecast/cli.rb
+++ lib/growthforecast/cli.rb
@@ -2,18 +2,15 @@
 require 'thor'
 
 class GrowthForecast::CLI < Thor
-  desc 'delete_graph <url>', 'delete a graph or graphs under a url'
+  desc 'delete <url>', 'delete a graph or graphs under a url'
   long_desc <<-LONGDESC
     Delete a graph or graphs under a <url> where <url> is the one obtained from the view, e.g., 
     http://{hostname}:{port}/list/{service_name}/{section_name}?t=sh
     or
     http://{hostname}:{port}/view_graph/{service_name}/{section_name}/{graph_name}?t=sh
   LONGDESC
-  def delete_graph(url)
-    uri = URI.parse(url)
-    client = client(uri)
-    service_name, section_name, graph_name = split_path(uri.path)
-    graphs = client.list_graph(service_name, section_name, graph_name)
+  def delete(url)
+    client, graphs = get_client_and_graphs(url)
     graphs.each do |graph|
       begin
         client.delete_graph(graph['service_name'], graph['section_name'], graph['graph_name'])
@@ -33,7 +30,47 @@ class GrowthForecast::CLI < Thor
     end
   end
 
+  desc 'color', 'change the color of graphs'
+  long_desc <<-LONGDESC
+    Change the color of graphs
+
+    ex)
+    growthforecast-client color -c "2xx_count:#1111cc" "3xx_count:#11cc11" "4xx_count:#cccc11" "5xx_count:#cc1111" \
+      -u 'http://{hostname}:{port}/list/{service_name}/{section_name}'
+  LONGDESC
+  option :colors, :type => :hash,   :aliases => '-c', :required => true
+  option :url,    :type => :string, :aliases => '-u', :required => true
+  def color
+    client, graphs = get_client_and_graphs(options[:url])
+    colors = options[:colors]
+    graphs.each do |graph|
+      service_name, section_name, graph_name = graph['service_name'], graph['section_name'], graph['graph_name']
+      next unless colors[graph_name]
+      params = {
+        'color'  => colors[graph_name],
+        'unit'   => 'count',
+        'sort'   => 1, # order to display, 19 is the top
+        'adjust' => '/',
+        'adjustval' => '1',
+      }
+      begin
+        puts "Setup #{service_name}/#{section_name}/#{graph_name} with #{colors[graph_name]}"
+        client.edit_graph(service_name, section_name, graph_name, params)
+      rescue GrowthForecast::NotFound => e
+        $stderr.puts "\tclass:#{e.class}\t#{e.message}"
+      end
+    end
+  end
+
   no_tasks do
+    def get_client_and_graphs(url)
+      uri = URI.parse(url)
+      client = client(uri)
+      service_name, section_name, graph_name = split_path(uri.path)
+      graphs = client.list_graph(service_name, section_name, graph_name)
+      [client, graphs]
+    end
+
     def e(str)
       CGI.escape(str).gsub('+', '%20') if str
     end
